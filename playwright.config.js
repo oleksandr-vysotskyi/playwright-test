cb16e16cf2afda1405c31ba599e081e3f0262f56
Vysotskyi, Oleksandr <oleksandr.v.vysotskyi@gmail.com>
2024-01-20 23:23:09 +0200

    updates

--- playwright.config.js
+++ playwright.config.js
@@ -30,9 +30,9 @@ export default defineConfig({
     baseURL: 'https://qauto.forstudy.space',
     httpCredentials: {
       // @ts-ignore
-      username: process.env.USER_EMAIL,
+      username: process.env.SITE_AUTH_USERNAME,
       // @ts-ignore
-      password: process.env.USER_PASSWORD,
+      password: process.env.SITE_AUTH_PASSWORD
     },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
